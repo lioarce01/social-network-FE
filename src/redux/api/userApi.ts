5849aca1027fd65aed7d2ad6091c18c43faa0e92
lioarce01 <lioarce1@gmail.com>
2024-12-22 09:09:47 -0300

    IMPL/ RTK Query user implementation

--- src/redux/api/userApi.ts
+++ src/redux/api/userApi.ts
@@ -9,7 +9,61 @@ export const userApi = createApi({
     getUsers: builder.query({
       query: () => "/users",
     }),
+    registerUser: builder.mutation({
+      query: (user) => {
+        return {
+          url: "/users",
+          method: "POST",
+          body: user,
+        };
+      },
+    }),
+    getUserBySub: builder.query({
+      query: (id) => `/users/${id}`,
+    }),
+    //PROFILE SETTINGS
+    updateUser: builder.mutation({
+      query: ({ id, user }) => {
+        return {
+          url: `/users/${id}`,
+          method: "PUT",
+          body: user,
+        };
+      },
+    }),
+    //PROFILE SETTINGS
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+    }),
+    //ONLY ADMIN CAN DISABLE USER
+    disableUser: builder.mutation({
+      query: (id) => ({
+        url: `/users/disable`,
+        method: "PUT",
+        body: id,
+      }),
+    }),
+    //ONLY ADMIN CAN SWITCH USER ROLE
+    switchUserRole: builder.mutation({
+      query: (id) => ({
+        url: `/users/switch-role`,
+        method: "PUT",
+        body: id,
+      }),
+    }),
   }),
 });
 
-export const { useGetUsersQuery } = userApi;
+export const {
+  useGetUsersQuery,
+  useGetUserBySubQuery,
+  useLazyGetUserBySubQuery,
+  useRegisterUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+  useDisableUserMutation,
+  useSwitchUserRoleMutation,
+} = userApi;
