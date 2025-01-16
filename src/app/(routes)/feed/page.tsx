e45d37cfd43ce3e2c11ab596e2af36c39c12068e
lioarce01 <lioarce1@gmail.com>
2025-01-16 18:13:23 -0300

    FIX/ feed posts dimensions

--- src/app/(routes)/feed/page.tsx
+++ src/app/(routes)/feed/page.tsx
@@ -5,7 +5,7 @@ export default function Feed() {
   return (
     <>
       <div className="w-full min-h-screen flex justify-center bg-gray-50">
-        <div className="space-y-2  md:min-w-[70%] lg:min-w-[50%] xl:min-w-[40%] pt-16 px-2">
+        <div className="space-y-2 pt-16 px-2">
           <WritePostComponent />
           <Posts />
         </div>
