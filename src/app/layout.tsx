a2ab83f695c34213c8721650c2bb4e3e50690807
lioarce01 <lioarce1@gmail.com>
2025-01-26 16:53:27 -0300

    fix/ make server component the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,24 @@
-"use client";
-
-import "./globals.css";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { useRouter } from "next/navigation";
+import "./globals.css";
 import { Providers } from "@/lib/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "TechConnect - Professional Network for Tech Experts",
-//   description: "Connect, collaborate, and grow in the tech industry",
-// };
+export const metadata: Metadata = {
+  title: "TechConnect - Professional Network for Tech Experts",
+  description: "Connect, collaborate, and grow in the tech industry",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const redirectUri =
-    typeof window !== "undefined" ? window.location.origin : "";
-
-  const router = useRouter();
-
-  const onRedirectCallback = (appState: any) => {
-    console.log("Redirecting to:", appState?.returnTo || "/");
-    router.push(appState?.returnTo?.toLowerCase() || "/");
-  };
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
-          <main>
-            <Auth0Provider
-              domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
-              clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
-              authorizationParams={{
-                redirect_uri: redirectUri,
-                audience: process.env.NEXT_PUBLIC_AUTH_AUDIENCE,
-                scope: "openid profile email",
-              }}
-              onRedirectCallback={onRedirectCallback}
-              cacheLocation="localstorage"
-            >
-              <Providers>{children}</Providers>
-            </Auth0Provider>
-          </main>
-        </div>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
